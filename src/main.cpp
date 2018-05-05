9909c00823ecd98d91a1484f013dcd0b2eebb804
Nina Tuluptceva <p.pin.p@yandex.ru>
2018-05-05 12:51:11 +0300

    Fix bug with linear space implementation

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,20 @@
 
 #define INF 1e20
 
+template <typename Head>
+void print(Head&& h)
+{
+    std::cout << std::forward<Head>(h) << std::endl;
+}
+
+template <typename Head, typename... Args>
+void print(Head&& h, Args&&... args)
+{
+    std::cout << std::forward<Head>(h) << " " ;
+    print(std::forward<Args>(args)...);
+}
+
+
 std::vector<std::vector<double>> readInputFile(const std::string &path) {
     std::ifstream data_file(path.c_str());
     std::vector<std::vector<double>> tss;
@@ -61,8 +75,9 @@ double dtw(const std::vector<double> &ts1, const std::vector<double> &ts2, int w
     dist[0][0] = 0;
 
     for(int i = 1; i <= n; i++) {
-//        for(int j = std::max<int>(1, i - ws); j <= std::min<int>(m, i + ws); j++) {
-        for(int j = 1; j <= m; j++) {
+        dist[i % 2][0] = INF;
+        for(int j = std::max<int>(1, i - ws); j <= std::min<int>(m, i + ws); j++) {
+//            for(int j = 1; j <= m; j++) {
             dist[i % 2][j] = cost(ts1[i], ts2[j]) +
                              std::min(std::min(dist[(i - 1) % 2][j], dist[i % 2][j - 1]), dist[(i - 1) % 2][j - 1]);
         }
@@ -80,56 +95,34 @@ double pruned_dtw(const std::vector<double> &ts1, const std::vector<double> &ts2
     }
 
     //create UB partial
-//    std::vector<double> ub_partial(n + 1);
-//    double last_ub = 0;
-//    if (n < m) {
-//        for(int i = n + 1; i <= m; i++) {
-//            last_ub += cost(ts1.at(n), ts2.at(i));
-//        }
-//    }
-//    for(int i = n; i >= 0; i--) {
-//        ub_partial.at(i) = last_ub;
-//        last_ub += cost(ts1.at(i), ts2.at(std::min(i, m)));
-//    }
-//    double ub = ub_partial[0];
-
     std::vector<double> ub_partial(n + 1);
     double last_ub = 0;
+    if (n < m) {
+        for(int i = n + 1; i <= m; i++) {
+            last_ub += cost(ts1.at(n), ts2.at(i));
+        }
+    }
     for(int i = n; i >= 0; i--) {
         ub_partial.at(i) = last_ub;
-        last_ub += cost(ts1.at(i), ts2.at(i));
+        last_ub += cost(ts1.at(i), ts2.at(std::min(i, m)));
     }
-
     double ub = ub_partial[0];
-    std::cout << ub << std::endl;
-//    if (n == m) {
-//        double tmp = 0;
-//        for(int i = 1; i <= n; i++){
-//            tmp += cost(ts1[i], ts2[i]);
-//        }
-//        if (std::abs(tmp - last_ub) > 1e-6) {
-//            std::cout << tmp << " " << last_ub;
-//            throw  std::exception();
-//        }
-//    }
 
     // contain only two vectors for odd and even rows
     std::vector<std::vector<double>> dist(2);
-    dist[0] = std::vector<double> (m + 1, INF);
-    dist[1] = std::vector<double> (m + 1, INF);
+    dist[0] = std::vector<double>(m + 1, INF);
+    dist[1] = std::vector<double>(m + 1, INF);
     dist[0][0] = 0;
 
     int sc = 1;
     int ec = 1;
-    double prev_ec = 1;
-
-    for(int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {
         bool smaler_found = false;
         int ec_next = i;
         dist[i % 2][sc - 1] = INF;
-//        for(int j = std::max<int>(sc, i - ws); j <= std::min<int>(m, i + ws); j++) {
-        for(int j = sc; j <= m; j++) {
-            if (j <= prev_ec) {
+        for(int j = std::max<int>(sc, i - ws); j <= std::min<int>(m, i + ws); j++) {
+//        for (int j = sc; j <= m; j++) {
+            if (j <= ec) {
                 dist[i % 2][j] = cost(ts1[i], ts2[j]) +
                                  std::min(std::min(dist[(i - 1) % 2][j], dist[i % 2][j - 1]), dist[(i - 1) % 2][j - 1]);
             } else {
@@ -149,9 +142,8 @@ double pruned_dtw(const std::vector<double> &ts1, const std::vector<double> &ts2
                 ec_next = j + 1;
             }
         }
-        prev_ec = ec;
         ec = ec_next;
-//        ub = dist[i % 2][i] + ub_partial[i];
+        ub = dist[i % 2][i] + ub_partial[i];
     }
     return dist[n % 2][m];
 }
@@ -183,22 +175,23 @@ int main(int argc, char *argv[]) {
 
     std::vector<std::vector<double>> tss = readInputFile(data_path);
     if (algorithm == "dtw") {
-        int i = 0, j = 1;
-        std::cout << tss.at(i).size() << " " <<  tss.at(j).size() << std::endl;
-        std::cout << "DTW:" << dtw(tss.at(i), tss.at(j), 300) << std::endl;
-        std::cout << "Pruned DTW:" << pruned_dtw(tss.at(i), tss.at(j), 300) << std::endl;
-//        for(int i = 0; i < tss.size(); i++) {
-//            for(int j = i + 1; j < tss.size(); j++) {
-//                double x = dtw(tss.at(i), tss.at(j));
-//                double y =  pruned_dtw(tss.at(i), tss.at(j));
-//                if (std::abs(x - y) > 1e-6) {
-//                    std::cout << i << " " << j << std::endl;
-//                    std::cout << "DTW:" <<  x << std::endl;
-//                    std::cout << "Pruned DTW:" << y << std::endl;
-////                    throw std::exception();
-//                }
-//            }
-//        }
+//        int i = 0, j = 2;
+//        std::cout << tss.at(i).size() << " " <<  tss.at(j).size() << std::endl;
+//        std::cout << "DTW:" << dtw(tss.at(i), tss.at(j), 300) << std::endl;
+//        std::cout << "Pruned DTW:" << pruned_dtw(tss.at(i), tss.at(j), 300) << std::endl;
+        for(int i = 0; i < tss.size(); i++) {
+            std::cout << i << std::endl;
+            for(int j = i + 1; j < tss.size(); j++) {
+                double x = dtw(tss.at(i), tss.at(j), 30);
+                double y =  pruned_dtw(tss.at(i), tss.at(j), 30);
+                if (std::abs(x - y) > 1e-6) {
+                    std::cout << i << " " << j << std::endl;
+                    std::cout << "DTW:" <<  x << std::endl;
+                    std::cout << "Pruned DTW:" << y << std::endl;
+//                    throw std::exception();
+                }
+            }
+        }
 
     } else if (algorithm == "pruned_dtw") {
         std::cout << "Pruned DTW:" << pruned_dtw(tss.at(0), tss.at(1)) << std::endl;
