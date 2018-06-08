0453db37e45131b4ed0db13a1800cd0808bdf5ab
DariaSav <31828817+DariaSav@users.noreply.github.com>
2018-06-08 16:09:36 +0300

    Add files via upload

--- dtw_all/all_double.sh
+++ dtw_all/all_double.sh
@@ -2,29 +2,16 @@ data_dir="../data/UCR_TS_Archive_2015/"
 files="Car CinC_ECG_torso Haptics InlineSkate MALLAT OliveOil"
 
 mkdir -p results/
-outfile=results/UCR_TS_Archive_2015.txt
-#echo "dataset | dtw | pruned_dtw | acceleration | sparce_dtw | acceleration" > ${outfile}
-
-dtw_sum=0
-pruned_dtw_sum=0
-sparce_dtw_sum=0
 
 for dataset in $files; do
     echo $dataset
-   # cmake-build-debug/src "dtw" "double" ${data_dir}/${dataset}/${dataset}_TRAIN ./results/dtw_res_${dataset}.txt
-    #dtw_sum=$(echo $dtw_sum + $dtw | bc)
+    cmake-build-debug/src "dtw" "double" ${data_dir}/${dataset}/${dataset}_TRAIN ./results/dtw_res_${dataset}.txt
 
-   # cmake-build-debug/src pruned_dtw double ${data_dir}/${dataset}/${dataset}_TRAIN ./results/pruned_dtw_res_${dataset}.txt
-   # pruned_dtw_sum=$(echo $pruned_dtw_sum + $pruned_dtw | bc)
+    cmake-build-debug/src pruned_dtw double ${data_dir}/${dataset}/${dataset}_TRAIN ./results/pruned_dtw_res_${dataset}.txt
 
-    cmake-build-debug/src sparce_dtw double ${data_dir}/${dataset}/${dataset}_TRAIN ./results/sparce2_dtw_res_${dataset}.txt
+    cmake-build-debug/src sparce_dtw double ${data_dir}/${dataset}/${dataset}_TRAIN ./results/sparce_dtw_res_${dataset}.txt
 
-   # cmake-build-debug/src fast_dtw double ${data_dir}/${dataset}/${dataset}_TRAIN ./results/fast_dtw_res_${dataset}.txt
-   # sparce_dtw_sum=$(echo $sparce_dtw_sum + $sparce_dtw | bc)
+    cmake-build-debug/src fast_dtw double ${data_dir}/${dataset}/${dataset}_TRAIN ./results/fast_dtw_res_${dataset}.txt
 
-   # echo "${dataset} | ${dtw} | ${pruned_dtw} | $(echo $dtw / $pruned_dtw | bc -l) | ${sparce_dtw} | $(echo $dtw / $sparce_dtw | bc -l)" >> ${outfile}
-   # echo "$dtw_sum  $pruned_dtw_sum $sparce_dtw_sum"
 done
 
-#echo ============================== >> ${outfile}
-#echo "SUM | $dtw_sum | $pruned_dtw_sum | $(echo $dtw_sum / $pruned_dtw_sum | bc -l) | $sparce_dtw_sum | $(echo $dtw_sum / $sparce_dtw_sum | bc -l)" >> ${outfile}
--- dtw_all/all_vector.sh
+++ dtw_all/all_vector.sh
@@ -2,8 +2,6 @@ data_dir="../data/SMD_Western-Music/vectors/"
 files="Brahms_Op034b-03.txt Brahms_Op108-01.txt Brahms_Op108-02.txt Brahms_Op108-03.txt Brahms_Op108-04.txt Chopin_Op060.txt Debussy_L140-01.txt Debussy_L140-02.txt Debussy_L140-03.txt Martin_Trio-02.txt Mozart_KV448-01.txt Rachmaninoff_Op005-01.txt Rachmaninoff_Op005-04.txt Ravel_PianoTrio-01.txt Ravel_PianoTrio-02.txt Ravel_PianoTrio-03.txt Ravel_PianoTrio-04.txt"
 
 mkdir -p results/
-outfile=results/UCR_TS_Archive_2015.txt
-#echo "dataset | dtw | pruned_dtw | acceleration | sparce_dtw | acceleration" > ${outfile}
 
 dtw_sum=0
 pruned_dtw_sum=0
@@ -11,20 +9,14 @@ sparce_dtw_sum=0
 
 for dataset in $files; do
     echo $dataset
-   # cmake-build-debug/src_vector "dtw" "vector" ${data_dir}/${dataset} ./results/dtw_${dataset}
-    #dtw_sum=$(echo $dtw_sum + $dtw | bc)
+    cmake-build-debug/srс "dtw" "vector" ${data_dir}/${dataset} ./results/dtw_${dataset}
 
-   # cmake-build-debug/src_vector pruned_dtw vector ${data_dir}/${dataset} ./results/pruned_dtw_${dataset}
-   # pruned_dtw_sum=$(echo $pruned_dtw_sum + $pruned_dtw | bc)
+    cmake-build-debug/src pruned_dtw vector ${data_dir}/${dataset} ./results/pruned_dtw_${dataset}
 
-    cmake-build-debug/src_vector sparce_dtw vector ${data_dir}/${dataset} ./results/sparce2_dtw_${dataset}
+    cmake-build-debug/src sparce_dtw vector ${data_dir}/${dataset} ./results/sparce2_dtw_${dataset}
 
-   # cmake-build-debug/src_vector fast_dtw vector ${data_dir}/${dataset} ./results/fast_dtw_${dataset}
-   # sparce_dtw_sum=$(echo $sparce_dtw_sum + $sparce_dtw | bc)
+    cmake-build-debug/src fast_dtw vector ${data_dir}/${dataset} ./results/fast_dtw_${dataset}
 
-   # echo "${dataset} | ${dtw} | ${pruned_dtw} | $(echo $dtw / $pruned_dtw | bc -l) | ${sparce_dtw} | $(echo $dtw / $sparce_dtw | bc -l)" >> ${outfile}
-   # echo "$dtw_sum  $pruned_dtw_sum $sparce_dtw_sum"
 done
 
-#echo ============================== >> ${outfile}
-#echo "SUM | $dtw_sum | $pruned_dtw_sum | $(echo $dtw_sum / $pruned_dtw_sum | bc -l) | $sparce_dtw_sum | $(echo $dtw_sum / $sparce_dtw_sum | bc -l)" >> ${outfile}
+
